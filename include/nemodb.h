f78515bd43ca1f4fb94aa4ec1949b17111257a0e
blrophon <gabriel.pequod@gmail.com>
2023-05-26 22:05:51 -0300

    Simple set and get KV

--- include/nemodb.h
+++ include/nemodb.h
@@ -1,11 +1,20 @@
 #ifndef NEMODB_h
 #define NEMODB_h
 
+#include <string.h>
+#include <stdlib.h>
+
 struct db{
     char *name;
     char *path;
 };
 
 struct db *DB_create(char *name);
+
+void DB_free(struct db *db);
+
+void DB_setKeyValue(char *key, char *val);
+
+char *DB_getKeyValue(char *key);
 
 #endif
--- src/nemodb.c
+++ src/nemodb.c
@@ -1,14 +1,18 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
 #include "nemodb.h"
-#include <string.h>
-#include <stdlib.h>
 //TODO: check errno for system calls
 
 
 int main(void){
-    DB_create("teste");
+    struct db *db = DB_create("teste");
+    DB_setKeyValue("key", "val");
+    char *val = DB_getKeyValue("key");
+    printf("%s\n", val);
+    free(val);
+    DB_free(db);
     return 0;
 }
 
@@ -25,7 +29,7 @@ struct db *DB_create(char *name){
     char dbfolder[64];
     strcat(dbfolder, basedir);
     strcat(dbfolder, "/");
-    strncat(dbfolder, name, 48);
+    strncat(dbfolder, name, 64 - strlen(basedir) + 1);
 
     if(stat(dbfolder, &st) < 0){ 
         mkdir(dbfolder, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);  //or 0700
@@ -36,4 +40,50 @@ struct db *DB_create(char *name){
     database->path = strdup(dbfolder);  //TODO: absolute path
                                         
     return database;
+}
+
+void DB_free(struct db *db){
+    free(db->name);
+    free(db->path);
+    free(db);
+}
+
+//Open nemodb/dbname/key.kv and store val
+void DB_setKeyValue(char *key, char *val){
+    char fname[64] = {0};
+    strncat(fname, key, 60);
+    strcat(fname, ".kv");
+
+    FILE *f = fopen(fname, "w");
+    fprintf(f, "%s", val);
+    fclose(f);
+}
+
+//Read all chars(in this case where the values are strings) of
+//nemodb/dbname/key_string.kv and return the complete string
+char *DB_getKeyValue(char *key){
+    char fname[64] = {0};
+    strncat(fname, key, 60);
+    strcat(fname, ".kv");
+
+    FILE *f = fopen(fname, "r");
+    if(f == 0){
+        printf("\nFile not found\n");
+        return NULL;
+    }
+
+    fseek(f, 0, SEEK_END);
+    int flength = ftell(f);
+    rewind(f);
+
+    char *val = malloc((flength+1) *sizeof(char));
+    char tmp;
+    int i;
+    for(i = 0; (tmp = fgetc(f)) != EOF; i++) {
+        val[i] = tmp;
+    }
+    val[i] = '\0';
+
+    fclose(f);
+    return val;
 }
